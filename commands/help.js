28937a4d596fc7d629dd8e96eec147972da2f841
The Casalegno <giuliocasalegno@libero.it>
2021-12-11 15:55:15 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -11,6 +11,8 @@ module.exports = {
             .setColor("GREEN")
             .setDescription("Ciao, sono il bot di Signal The Frog, questi sono i miei (pochi) comandi che puoi utilizzare")
             .addField(":compass: USERSTATS :compass:", "Usa questo comando per vedere le tue statistiche tramite **Project-GC** \n *Per un utilizzo ottimale, ti consiglio di modificare il tuo nickname nel server, come quello di Geocaching!* \n **|** `[!stats]` **|** `[!userstats]` **|** `[!statistiche]` **|**", false )
+            .addField(":compass: BADGEBAR :compass:", "Usa questo comando per vedere i tuoi badge di **Project-GC** \n *Per un utilizzo ottimale, ti consiglio di modificare il tuo nickname nel server, come quello di Geocaching!* \n **|** `[!badgebar]` **|** `[!badge]` **|** `[!badgen]` **|**", false )
+            .addField(":compass: PROFILE :compass:", "Usa questo comando per ottenere il link del tuo profilo **Geocaching** \n *Per un utilizzo ottimale, ti consiglio di modificare il tuo nickname nel server, come quello di Geocaching!* \n **|** `[!profile]` **|** `[!profilo]` **|** `[!p]` **|**", false )
 
         message.channel.send({ embeds: [embed] })
     }
