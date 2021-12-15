8cbb7765c027da8827df158d120b2c980c2dad3f
The Casalegno <giuliocasalegno@libero.it>
2021-12-15 20:25:07 +0100

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -9,7 +9,7 @@ module.exports = {
         const embed = new Discord.MessageEmbed()
             .setTitle(":link: Bot invitation link :link:")
             .setColor("GREEN")
-            .setDescription("Here is the ** invite link ** to add this bot to your server:\n__https://discord.com/oauth2/authorize?client_id=919001856263487488&scope=bot&permissions=274878220288__")
+            .setDescription("Here is the ** invite link ** to add this bot in your server:\n__https://discord.com/oauth2/authorize?client_id=919001856263487488&scope=bot&permissions=274878220288__")
 
         message.channel.send({ embeds: [embed] })
     }
